ab82a8a69951597d35f572b81092ae3f4a587b42
PMayukh <48017535+PMayukh@users.noreply.github.com>
2021-07-10 13:55:21 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 @SpringBootTest
 class DemoApplicationTests {
